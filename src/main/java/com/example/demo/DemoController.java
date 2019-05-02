431a1dc2bcb8295e5316a1a82e02d549c050d00b
lemon <924438567@qq.com>
2019-05-02 17:35:30 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class DemoController {
+
+    @GetMapping("/myDemo")
+    public String test(){
+        System.out.println("fhfshgdfsjfgshfdshj");
+        return "1111";
+    }
+}
